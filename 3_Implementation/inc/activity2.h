d47ad9a6186e8224f1a725dff69e26bbfcef1e8d
Harshithan-123 <94290462+Harshithan-123@users.noreply.github.com>
2021-11-30 20:19:51 +0530

    Delete 3_Implementation directory

--- 3_Implementation/inc/activity2.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACTIVITY2_H_INCLUDED
-#define ACTIVITY2_H_INCLUDED
-
-void InitADC();
-uint16_t ReadADC(uint8_t ch);
-
-#endif
-
--- 3_Implementation/inc/activity3.h
+++
@@ -1,7 +0,0 @@
-#ifndef ACTIVITY3_H_INCLUDED
-#define ACTIVITY3_H_INCLUDED
-
-void config_timer(void);
-void pwm_output(void);
-
-#endif
